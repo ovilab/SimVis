f3515788524ce572052495da0baaa54060ff809f
Svenn-Arne Dragly <s@dragly.com>
2015-03-15 00:58:20 +0100

    Fixed import path

--- examples/footballs/main.cpp
+++ examples/footballs/main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    engine.addImportPath("/home/svenni/Dropbox/projects/programming/compphys/build-SimVis-Desktop_Qt_5_4_0_GCC_64bit-Debug/src");
+    engine.addImportPath("../../src");
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
 
     return app.exec();
