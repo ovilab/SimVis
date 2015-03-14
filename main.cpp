77ed6adf8cfcf4aae5953ab5f3d581567dd1dcab
Anders Hafreager <anders@hafreager.no>
2015-03-14 19:34:20 +0100

    Fixed so Simulator is registered as an interface in QML.

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 
 int main(int argc, char *argv[])
 {
-    qRegisterMetaType<MySimulator*>();
+    qmlRegisterInterface<Simulator>("Simulator");
     qmlRegisterType<MySimulator>("MySimulator", 1, 0, "MySimulator");
     qmlRegisterType<Billboards2D>("MySimulator", 1, 0, "Billboards2D");
     qmlRegisterType<Visualizer>("MySimulator", 1, 0, "Visualizer");
