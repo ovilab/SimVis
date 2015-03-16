35602dc28b89781425bcabc081c3acdc5e2ef1ce
Svenn-Arne Dragly <s@dragly.com>
2015-03-16 11:58:10 +0100

    Added missing navigator and worker files

---
+++ src/navigator.cpp
@@ -0,0 +1,27 @@
+//#include "navigator.h"
+//#include "camera.h"
+
+//Navigator::Navigator()
+//{
+
+//}
+
+//Navigator::~Navigator()
+//{
+
+//}
+
+//Camera *Navigator::camera() const
+//{
+//    return m_camera;
+//}
+
+//void Navigator::setCamera(Camera *arg)
+//{
+//    if (m_camera == arg)
+//        return;
+
+//    m_camera = arg;
+//    emit cameraChanged(arg);
+//}
+
