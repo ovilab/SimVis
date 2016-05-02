3b00ee6b69e32a1348673a06e8c7c5af2770d899
Svenn-Arne Dragly <s@dragly.com>
2016-05-02 17:07:56 +0200

    Qt3D refactoring updates for Qt5.7

--- src/utils/marchingcubestabletexture.h
+++ src/utils/marchingcubestabletexture.h
@@ -1,18 +1,18 @@
 #ifndef MARCHINGCUBESTABLETEXTURE_H
 #define MARCHINGCUBESTABLETEXTURE_H
 #include <QAbstractTextureImage>
-#include <QTexture2D>
 #include <QTexImageData>
+#include <Qt3DRender/QTexture>
 #include <QImage>
 #include <QColor>
 
-class MCDataFunctor : public Qt3DRender::QTextureDataFunctor {
+class MCDataFunctor : public Qt3DRender::QTextureImageDataGenerator {
     // QAbstractFunctor interface
 public:
-    MCDataFunctor() : Qt3DRender::QTextureDataFunctor() { }
+    MCDataFunctor() : Qt3DRender::QTextureImageDataGenerator() { }
     ~MCDataFunctor() {}
     virtual Qt3DRender::QTexImageDataPtr operator ()() override;
-    virtual bool operator ==(const Qt3DRender::QTextureDataFunctor &other) const override;
+    virtual bool operator ==(const Qt3DRender::QTextureImageDataGenerator &other) const override;
     QT3D_FUNCTOR(MCDataFunctor)
 };
 
@@ -26,7 +26,7 @@ private:
     QT3D_CLONEABLE(MarchingCubesTableTexture)
     // QAbstractTextureImage interface
 public:
-    virtual Qt3DRender::QTextureDataFunctorPtr dataFunctor() const override;
+    virtual Qt3DRender::QTextureImageDataGeneratorPtr dataGenerator() const override;
 };
 
 #endif // MARCHINGCUBESTABLETEXTURE_H
